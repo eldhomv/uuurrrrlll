175c3743d830d412b0e1aad8ff35cae71627b026
eldhomv <64636539+eldhomv@users.noreply.github.com>
2021-02-14 12:30:07 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,7 +8,6 @@ This is a Telegram URL Upload Bot!
 
 /help for more details..
 
-Support Group : @TroJanzSupport
 """
 
     HELP_USER = """Hai I'am a URL Uploader bot..
@@ -21,7 +20,6 @@ Support Group : @TroJanzSupport
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
 
-Support Group : @TroJanzSupport
 """
 
     FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
